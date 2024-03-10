e205a97bcc40e7da8b4e8862cc1df9541bcd29b7
iAmGiG <gigachris1@hotmail.com>
2024-03-10 13:43:17 -0400

    seeing if the tokens.h will help

---
+++ tokens.h
@@ -0,0 +1,14 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#define T_IDENTIFIER 1001
+#define T_INTEGER_LITERAL 1002
+#define T_DOUBLE_LITERAL 1003
+#define T_STRING_LITERAL 1004
+#define T_BOOLEAN_LITERAL 1005
+#define T_VOID 1006
+#define T_INT 1007
+#define T_DOUBLE 1008
+// Continue for other tokens...
+
+#endif
